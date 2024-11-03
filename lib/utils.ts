73dddd9b3e5aad9aab03cabfac6f132c017381d6
PankajKumar Yadav <pankaj981966@gmail.com>
2024-11-03 16:50:16 +0530

    feat: function for calculating reading time

---
+++ lib/utils.ts
@@ -0,0 +1,13 @@
+export function calculateReadingTime(mdxContent: any) {
+  // Define the average reading speed (words per minute)
+  const wordsPerMinute = 200
+
+  // Strip MDX/HTML tags and count the words
+  const text = mdxContent.replace(/<\/?[^>]+(>|$)/g, '')
+  const wordCount = text.split(/\s+/).filter((word: any) => word.length > 0).length
+
+  // Calculate reading time
+  const readingTime = Math.ceil(wordCount / wordsPerMinute)
+
+  return readingTime
+}
