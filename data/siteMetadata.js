df2f13b4c4fa8724a187a8b64fd6d1fd9ddda88d
PankajKumar Yadav <pankaj981966@gmail.com>
2024-12-18 20:21:46 +0530

    chore: added the repo for comment

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: 'Full-stack Developer specializing in the MERN stack',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://pankajkumaryadav.vercel.app',
+  siteRepo: 'https://github.com/pankaj-kumar-yadav/portfolio-2',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
